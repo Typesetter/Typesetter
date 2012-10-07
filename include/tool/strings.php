fa4a69f7af4b3b0050af599462ced449b5cade09
Josh Schmidt <oyejorge@gmail.com>
2012-10-07 12:12:03 -0700

    moved mb_internal_encoding to common.php

--- include/tool/strings.php
+++ include/tool/strings.php
@@ -2,13 +2,6 @@
 defined('is_running') or die('Not an entry point...');
 
 
-/*
-	see /var/www/wikyblog/burlington/include/wiki2.php for mb_* string functions
-*/
-if( function_exists('mb_internal_encoding') ){
-	mb_internal_encoding('UTF-8');
-}
-
 class gp_strings{
 
 	/**

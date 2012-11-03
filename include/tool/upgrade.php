d9ad6b58da73def333e3065d7c7fea6137655909
Josh Schmidt <oyejorge@gmail.com>
2012-11-02 17:30:49 -0700

    show current version in update prompts

--- include/tool/upgrade.php
+++ include/tool/upgrade.php
@@ -11,15 +11,15 @@ class gpupgrade{
 
 
 		if( version_compare($config['gpversion'],'1.6','<') ){
-			die('Please upgrade to version 1.6, then 1.7 before upgrading to this version.');
+			die('Please upgrade to version 1.6, then 1.7 before upgrading to this version. You current version is '.$config['gpversion']);
 		}
 
 		if( version_compare($config['gpversion'],'1.7a2','<') ){
-			die('Please upgrade to version 1.7 before upgrading to this version.');
+			die('Please upgrade to version 1.7 before upgrading to this version. You current version is '.$config['gpversion']);
 		}
 
 		if( version_compare($config['gpversion'],'1.8a1','<') ){
-			die('Please upgrade to version 2.0 before upgrading to this version.');
+			die('Please upgrade to version 2.0 before upgrading to this version. You current version is '.$config['gpversion']);
 		}
 
 		if( version_compare($config['gpversion'],'2.3.4','<') ){

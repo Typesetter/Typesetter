941ca8d020a6c6b3264ca8abdd69be75e094d694
Josh Schmidt <oyejorge@gmail.com>
2012-12-20 19:57:21 -0700

    format errors

--- include/admin/admin_errors.php
+++ include/admin/admin_errors.php
@@ -23,10 +23,63 @@ class admin_errors{
 			return;
 		}
 
-		echo '<pre>';
-		echo file_get_contents($error_log);
+		$contents = file_get_contents($error_log);
+		$lines = trim($contents);
+		$lines = explode("\n",$contents);
+		$lines = array_reverse($lines);
+
+		$time = null;
+		$displayed = array();
+		foreach($lines as $line){
+			$line = trim($line);
+			if( empty($line) ){
+				continue;
+			}
+			preg_match('#^\[[a-zA-Z0-9:\- ]*\]#',$line,$date);
+			if( count($date) ){
+				$date = $date[0];
+				$line = substr($line,strlen($date));
+				$date = trim($date,'[]');
+				$new_time = strtotime($date);
+				if( $new_time !== $time ){
+					if( $time ){
+						echo '</pre>';
+					}
+					echo self::Elapsed( time() - $new_time ).' ago ('.$date.')';
+					echo '<pre>';
+					$time = $new_time;
+					$displayed = array();
+				}
+			}
+
+
+			$line_hash = md5($line);
+			if( in_array($line_hash,$displayed) ){
+				continue;
+			}
+			echo $line;
+			$displayed[] = $line_hash;
+			echo "\n";
+		}
 		echo '</pre>';
 
+	}
+
+	static function Elapsed($difference){
+		$periods = array("second", "minute", "hour", "day", "week", "month", "year", "decade");
+		$lengths = array("60","60","24","7","4.35","12","10");
+
+		for($j = 0; $difference >= $lengths[$j] && $j < count($lengths)-1; $j++) {
+		   $difference /= $lengths[$j];
+		}
+
+		$difference = round($difference);
+
+		if($difference != 1) {
+		   $periods[$j].= "s";
+		}
+
+		return $difference.' '.$periods[$j];
 	}
 
 }

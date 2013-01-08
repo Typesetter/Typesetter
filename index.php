7c9fbd7c63efe327e423b1b6f013e09ef5b3d132
Josh Schmidt <oyejorge@gmail.com>
2013-01-08 10:43:02 -0700

    add sanity check to index.php

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
 
 /**
+ * Make sure php is running
+ *
+ */
+if( false ){
+	?>
+	<!DOCTYPE html>
+	<html>
+	<head>
+	<meta charset="UTF-8" />
+	<title>Error: PHP is not running</title>
+	</head>
+	<body>
+	<h1><a href="http://gpeasy.com/">gpEasy</a></h1>
+	<h2>Error: PHP is not running</h2>
+	<p>gpEasy requires that your web server is running PHP. Your server does not have PHP installed, or PHP is turned off.</p>
+	</body>
+	</html>
+	<?php
+}
+
+
+/**
  * See gpconfig.php for configuration options
  *
  */

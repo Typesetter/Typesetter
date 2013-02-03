3a31bc8391a41a57f82e4cc9be8f642c9c889024
Josh Schmidt <oyejorge@gmail.com>
2013-02-02 21:08:46 -0700

    add admin_tools::base_convert

--- include/admin/admin_tools.php
+++ include/admin/admin_tools.php
@@ -1293,4 +1293,33 @@ class admin_tools{
 		return round(pow(1024, $base - $floor), $precision) .' '. $suffixes[$floor];
 	}
 
+	/**
+	 * Base convert that handles large numbers
+	 *
+	 */
+	static function base_convert($str, $frombase=10, $tobase=36) {
+	    $str = trim($str);
+	    if (intval($frombase) != 10) {
+	        $len = strlen($str);
+	        $q = 0;
+	        for ($i=0; $i<$len; $i++) {
+	            $r = base_convert($str[$i], $frombase, 10);
+	            $q = bcadd(bcmul($q, $frombase), $r);
+	        }
+	    }
+	    else $q = $str;
+
+	    if (intval($tobase) != 10) {
+	        $s = '';
+	        while (bccomp($q, '0', 0) > 0) {
+	            $r = intval(bcmod($q, $tobase));
+	            $s = base_convert($r, 10, $tobase) . $s;
+	            $q = bcdiv($q, $tobase, 0);
+	        }
+	    }
+	    else $s = $q;
+
+	    return $s;
+	}
+
 }

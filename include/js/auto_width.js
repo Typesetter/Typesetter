100224be6fa84093bfb10bbc96208c9c9a552b74
Josh Schmidt <oyejorge@gmail.com>
2013-04-08 00:19:23 -0600

    add auto_width.js

---
+++ include/js/auto_width.js
@@ -0,0 +1,44 @@
+
+
+
+$(function(){
+
+	//auto width
+	var $area_wrap = $('#adminlinks2');
+	var adjust = 22;
+	var min_width = 280;
+	var width_2 = 600;
+	var width_3 = 900;
+
+	$('#admincontainer').resize(function(){
+
+		var width = $area_wrap.width();
+		var cols = 1;
+
+		if( width > width_3 ){
+			cols = 3;
+		}else if( width > width_2 ){
+			cols = 2;
+		}
+		$area_wrap.children('div').width( (width/cols)-adjust );
+
+	}).resize();
+
+
+
+	//expand_child_click
+	$(document).on('click','.expand_child_click',function(evt){
+		evt.preventDefault();
+		var $this = $(this);
+		if( $this.hasClass('expand') ){
+			$this.parent().children().removeClass('expand');
+		}else{
+			$this.siblings().removeClass('expand');
+			$this.addClass('expand');
+		}
+	});
+	$(document).on('click','.expand_child_click li',function(evt){
+		evt.stopPropagation();
+	});
+
+});

b7266f4ff28d85492ce4d0ee72c28d6a0c361d21
Josh Schmidt <oyejorge@gmail.com>
2013-04-25 18:35:39 -0600

    remove unused files

--- include/install/install_tools.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('is_running') or die('Not an entry point...');
-
-/*
- * install_tool code moved to tool/install.php, /install/install_tools.php will be deleted
- */
-trigger_error('use /tool/install.php instead of /install/install_tools.php');
-includeFile('tool/install.php');

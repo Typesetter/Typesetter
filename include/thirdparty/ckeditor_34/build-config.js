9dd128e6492f2e0ec895578a6a960f2185db8236
Josh Schmidt <oyejorge@gmail.com>
2014-01-30 22:54:40 -0700

    update ckeditor to 4.3.2 with explorer 11 support

--- include/thirdparty/ckeditor_34/build-config.js
+++ include/thirdparty/ckeditor_34/build-config.js
@@ -1,14 +1,25 @@
-﻿
-/**
- * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+﻿/**
+ * @license Copyright (c) 2003-2014, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
  */
 
 /**
  * This file was added automatically by CKEditor builder.
- * You may re-use it at any time at http://ckeditor.com/builder to build CKEditor again.
- * 
- * NOTE: 
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) http://ckeditor.com/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) http://ckeditor.com/builder/c200ef09b560d8e27bced2f72590a6c1
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) http://ckeditor.com/builder/download/c200ef09b560d8e27bced2f72590a6c1
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
  *    This file is not used by CKEditor, you may remove it.
  *    Changing this file will not change your CKEditor configuration.
  */
@@ -25,88 +36,88 @@ var CKBUILDER_CONFIG = {
 	],
 	plugins : {
 		'a11yhelp' : 1,
-		'dialogadvtab' : 1,
 		'basicstyles' : 1,
 		'bidi' : 1,
 		'blockquote' : 1,
+		'button' : 1,
 		'clipboard' : 1,
 		'colorbutton' : 1,
 		'colordialog' : 1,
-		'templates' : 1,
 		'contextmenu' : 1,
+		'dialog' : 1,
+		'dialogadvtab' : 1,
+		'dialogui' : 1,
 		'div' : 1,
-		'resize' : 1,
-		'toolbar' : 1,
 		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
+		'fakeobjects' : 1,
 		'filebrowser' : 1,
 		'find' : 1,
 		'flash' : 1,
+		'floatpanel' : 1,
 		'font' : 1,
-		'forms' : 1,
 		'format' : 1,
-		'htmlwriter' : 1,
+		'forms' : 1,
 		'horizontalrule' : 1,
+		'htmlwriter' : 1,
 		'iframe' : 1,
-		'wysiwygarea' : 1,
 		'image' : 1,
 		'indent' : 1,
-		'smiley' : 1,
+		'indentlist' : 1,
 		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
+		'listblock' : 1,
 		'liststyle' : 1,
 		'maximize' : 1,
+		'menu' : 1,
 		'newpage' : 1,
 		'pagebreak' : 1,
-		'pastetext' : 1,
+		'panel' : 1,
+		'panelbutton' : 1,
 		'pastefromword' : 1,
+		'pastetext' : 1,
+		'popup' : 1,
 		'removeformat' : 1,
+		'resize' : 1,
+		'richcombo' : 1,
 		'save' : 1,
 		'selectall' : 1,
+		'sharedspace' : 1,
 		'showblocks' : 1,
 		'showborders' : 1,
+		'smiley' : 1,
 		'sourcearea' : 1,
+		'sourcedialog' : 1,
 		'specialchar' : 1,
 		'stylescombo' : 1,
+		'stylesheetparser' : 1,
 		'tab' : 1,
 		'table' : 1,
-		'tabletools' : 1,
-		'undo' : 1,
-		'dialog' : 1,
-		'dialogui' : 1,
-		'panelbutton' : 1,
-		'button' : 1,
-		'floatpanel' : 1,
-		'panel' : 1,
-		'menu' : 1,
-		'popup' : 1,
-		'fakeobjects' : 1,
-		'richcombo' : 1,
-		'listblock' : 1,
-		'stylesheetparser' : 1,
 		'tableresize' : 1,
-		'sourcedialog' : 1,
-		'sharedspace' : 1,
-		'indentlist' : 1
+		'tabletools' : 1,
+		'templates' : 1,
+		'toolbar' : 1,
+		'undo' : 1,
+		'wysiwygarea' : 1
 	},
 	languages : {
 		'af' : 1,
 		'ar' : 1,
 		'bg' : 1,
 		'ca' : 1,
-		'hr' : 1,
 		'cs' : 1,
 		'da' : 1,
-		'et' : 1,
 		'de' : 1,
 		'el' : 1,
 		'en' : 1,
 		'es' : 1,
+		'et' : 1,
 		'fi' : 1,
 		'fr' : 1,
 		'gl' : 1,
+		'hr' : 1,
 		'hu' : 1,
 		'it' : 1,
 		'ja' : 1,
@@ -114,14 +125,14 @@ var CKBUILDER_CONFIG = {
 		'nl' : 1,
 		'no' : 1,
 		'pl' : 1,
-		'pt-br' : 1,
 		'pt' : 1,
+		'pt-br' : 1,
 		'ru' : 1,
 		'sk' : 1,
 		'sl' : 1,
 		'sv' : 1,
 		'tr' : 1,
 		'uk' : 1,
-		'zh' : 1,
+		'zh' : 1
 	}
 };

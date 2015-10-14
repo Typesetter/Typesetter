96d4ca13af6217f2fb4c0861765f31e82f3ef074
Josh Schmidt <oyejorge@gmail.com>
2015-10-13 18:37:42 -0600

    remove links to outdated projects

--- include/tool/install.php
+++ include/tool/install.php
@@ -302,11 +302,6 @@ class Install_Tools{
 		<li>Fast and Easy Installation</li>
 		<li>reCaptcha for Contact Form</li>
 		<li>HTML Tidy (when available)</li>
-		</ul>
-		<h2>If You Like gpEasy...</h2>
-		<p>If you like gpEasy, then you might also like:</p>
-		<ul>
-		<li><a href="http://phpeasymin.com" title="Minimize JavaScript and CSS files easily">phpEasyMin.com</a> - Minimize multiple JavaScript and CSS files in one sweep.</li>
 		</ul>');
 
 		//Side_Menu
@@ -338,8 +333,6 @@ class Install_Tools{
 		<p><a href="http://gpeasy.com/Our_CMS" title="Features of Our CMS">And More...</a></p>
 		<p>If you like gpEasy, then you might also like
 		<a href="http://lessphp.gpeasy.com" title="A Less to CSS compiler based on the official lesscss project">Less.php</a>,
-		<a href="http://gpfinder.org" title="gpFinder is a free and open-source AJAX file manager">gpFinder.org</a>,
-		<a href="http://phpeasymin.com" title="Minimize JavaScript and CSS files easily">phpEasyMin.com</a>,
 		<a href="http://whatcms.org" title="What CMS? Find out what CMS a site is using">WhatCMS.org</a> and
 		<a href="http://whichcms.org" title="Which CMS? Find out which CMS has the features you\'re looking for.">WhichCMS.org</a>.
 		</p>';

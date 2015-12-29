e1f03d49a8a7031a9d62559292295a5c514d40f1
josh <oyejorge@gmail.com>
2015-12-28 23:50:05 -0700

    textarea width

--- include/admin/Configuration.php
+++ include/admin/Configuration.php
@@ -405,7 +405,7 @@ class Configuration{
 		global $langmessage;
 		$count_label = sprintf($langmessage['_characters'],'<span>'.strlen($value).'</span>');
 		echo '<span class="show_character_count gptextarea">';
-		echo '<textarea id="'.$name.'" name="'.$name.'" cols="40" rows="2">'.htmlspecialchars($value).'</textarea>';
+		echo '<textarea id="'.$name.'" name="'.$name.'" cols="50" rows="2">'.htmlspecialchars($value).'</textarea>';
 		echo '<span class="character_count">'.$count_label.'</span>';
 		echo '</span>';
 	}

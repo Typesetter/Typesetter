eb84d4c8f864bb82ee174a813524477112fb0d6e
josh <oyejorge@gmail.com>
2015-12-29 20:08:44 -0700

    add .gp-fixed-adjust to flatly theme

--- themes/Bootswatch_Flatly/2_Jumbotron/template.php
+++ themes/Bootswatch_Flatly/2_Jumbotron/template.php
@@ -25,7 +25,7 @@ include_once($path);
 
 
 	<body>
-		<div class="navbar navbar-default navbar-fixed-top">
+		<div class="navbar navbar-default navbar-fixed-top gp-fixed-adjust">
 			<div class="container">
 				<div class="navbar-header">
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
--- themes/Bootswatch_Flatly/4_Sticky_Footer/template.php
+++ themes/Bootswatch_Flatly/4_Sticky_Footer/template.php
@@ -28,7 +28,7 @@ include_once($path);
 
 	    <!-- Wrap all page content here -->
 	    <div id="wrap">
-			<div class="navbar navbar-default navbar-fixed-top">
+			<div class="navbar navbar-default navbar-fixed-top gp-fixed-adjust">
 				<div class="container">
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">

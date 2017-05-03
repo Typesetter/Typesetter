0d558c08351d2f6bf72e73220905a06caed1ba0b
juek <jk@grafikrausz.at>
2017-05-03 21:04:32 +0200

    update index.php

--- index.php
+++ index.php
@@ -13,14 +13,17 @@ if( false ){
 	<title>Error: PHP is not running</title>
 	</head>
 	<body>
-	<h1><a href="http://gpeasy.com/">gpEasy</a></h1>
+	<h1><a href="https://www.typesettercms.com/">Typesetter CMS</a></h1>
 	<h2>Error: PHP is not running</h2>
-	<p>gpEasy requires that your web server is running PHP. Your server does not have PHP installed, or PHP is turned off.</p>
+	<p>Typesetter requires that your web server is running PHP. Your server does not have PHP installed, or PHP is turned off.</p>
 	</body>
 	</html>
 	<?php
 }
 
+if( version_compare(phpversion(), '5.3.0', '<') ){
+	exit( 'Error: Typesetter CMS requires PHP 5.3+. This server is running PHP version ' . phpversion() );
+}
 
 /**
  * See gpconfig.php for configuration options

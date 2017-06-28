a5e4052cf6cf024b8f6cad8389634cf2cbb41d32
juek <jk@grafikrausz.at>
2017-06-28 18:48:39 +0200

    change 'gpEasy' to 'Typesetter'
    some leftovers

--- addons/Example/Admin.php
+++ addons/Example/Admin.php
@@ -10,15 +10,15 @@ class Admin{
 	function __construct(){
 		echo '<h2>This is an Admin Only Script</h2>';
 
-		echo '<p>This is an example of a gpEasy Addon in the form of a Admin page.</p>';
+		echo '<p>This is an example of a Typesetter Addon in the form of a Admin page.</p>';
 
-		echo '<p>Admin pages are only accessible to users with appropriate permissions on your installation of gpEasy. </p>';
+		echo '<p>Admin pages are only accessible to users with appropriate permissions on your installation of Typesetter CMS. </p>';
 
 		echo '<p>';
 		echo \common::Link('Special_Example','An Example Link');
 		echo '</p>';
 
-		echo '<p>You can download <a href="http://gpeasy.com/Special_Addon_Plugins?id=160">a plugin with addtional examples</a> from gpEasy.com </p>';
+		echo '<p>You can download <a href="https://www.typesettercms.com/Special_Addon_Plugins?id=160">a plugin with addtional examples</a> from typesettercms.com </p>';
 	}
 }
 

3b466b9e0e9e2ccb1500e571a7441dcac622043a
juek <jk@grafikrausz.at>
2017-08-09 15:29:28 +0200

    prevent iframe nesting in Layout Editor
    fix for issue https://github.com/Typesetter/Typesetter/issues/206

--- include/js/theme_content_outer.js
+++ include/js/theme_content_outer.js
@@ -7,9 +7,15 @@ $(function(){
 	 */
 	var iframe	= document.getElementById('gp_layout_iframe');
 	if( iframe ){
+
+		if ( iframe.contentWindow.document.getElementById('gp_layout_iframe') ){
+			// prevent nested iframes
+			inner_iframe_src = iframe.contentWindow.document.getElementById('gp_layout_iframe').getAttribute("src");
+			iframe.src = inner_iframe_src;
+		}
+
 		var $wrap	= $('#gp_iframe_wrap');
 		window.setInterval(function(){
-
 			var iframe		= document.getElementById('gp_layout_iframe');
 			var body		= iframe.contentWindow.document.body;
 			if( body ){

e851c39db6a9af30f9c4c419674e7b505ad6f8a0
juek <jk@grafikrausz.at>
2018-05-18 03:41:34 +0200

    add fonts to $allowed_types

--- include/admin/Content/Uploaded.php
+++ include/admin/Content/Uploaded.php
@@ -671,6 +671,7 @@ namespace gp\admin\Content{
 						/** Media **/		'aiff', 'asf', 'avi', 'fla', 'flac', 'flv', 'm4v', 'mid', 'mov', 'mp3', 'mp4', 'mpc', 'mpeg', 'mpg', 'ogg', 'oga', 'ogv', 'opus', 'qt', 'ram', 'rm', 'rmi', 'rmvb', 'swf', 'wav', 'wma', 'webm', 'wmv', 
 						/** Archives **/	'7z', 'bz', 'gz', 'gzip', 'rar', 'tar', 'tgz', 'zip',
 						/** Text/Docs **/	'css', 'csv', 'doc', 'docx', 'htm', 'html', 'js', 'json', 'less', 'md', 'ods', 'odt', 'pages', 'pdf', 'ppt', 'pptx', 'rtf', 'txt', 'scss', 'sxc', 'sxw', 'vsd', 'xls', 'xlsx', 'xml', 'xsl', 
+						/** Fonts **/		'eot', 'otf', 'ttf', 'woff', 'woff2', 
 					);
 
 

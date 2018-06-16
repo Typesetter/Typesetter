d7d1ccb97b1059d3bedc5052d2bb4c22f5ccc65d
juek <jk@grafikrausz.at>
2018-06-16 18:03:36 +0200

    add section label localization for 'File Include'

--- include/tool/Editing.php
+++ include/tool/Editing.php
@@ -713,6 +713,7 @@ namespace gp\tool{
 
 			switch($type){
 				case 'include':
+					$section['gp_label']					= $langmessage['File Include'];
 				break;
 
 				case 'gallery':
@@ -741,8 +742,11 @@ namespace gp\tool{
 				break;
 
 				case 'text':
-				default:
 					$section['gp_label']				= $langmessage['editable_text'];
+					$section['content']					= '<div><h2>'.strip_tags($heading).'</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>';
+				break;
+
+				default:
 					$section['content']					= '<div><h2>'.strip_tags($heading).'</h2><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p><p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>';
 				break;
 			}

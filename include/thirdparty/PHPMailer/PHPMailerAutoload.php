b9444fb7d1c9dfcad5b965904f9c9721e6a5726e
juek <jk@grafikrausz.at>
2019-11-09 00:14:46 +0100

    remove deprecated autoloading

--- include/thirdparty/PHPMailer/PHPMailerAutoload.php
+++ include/thirdparty/PHPMailer/PHPMailerAutoload.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * PHPMailer SPL autoloader.
- * PHP Version 5
+ * PHP Version 5.4+
  * @package PHPMailer
  * @link https://github.com/PHPMailer/PHPMailer/ The PHPMailer GitHub project
  * @author Marcus Bointon (Synchro/coolbru) <phpmailer@synchromedia.co.uk>
@@ -21,31 +21,11 @@
  * PHPMailer SPL autoloader.
  * @param string $classname The name of the class to load
  */
-function PHPMailerAutoload($classname)
-{
-    //Can't use __DIR__ as it's only in PHP 5.3+
-    $filename = dirname(__FILE__).DIRECTORY_SEPARATOR.'class.'.strtolower($classname).'.php';
-    if (is_readable($filename)) {
-        require $filename;
-    }
+function PHPMailerAutoload($classname){
+  $filename = __DIR__ . DIRECTORY_SEPARATOR . 'class.' . strtolower($classname) . '.php';
+  if (is_readable($filename)) {
+    require $filename;
+  }
 }
 
-if (version_compare(PHP_VERSION, '5.1.2', '>=')) {
-    //SPL autoloading was introduced in PHP 5.1.2
-    if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
-        spl_autoload_register('PHPMailerAutoload', true, true);
-    } else {
-        spl_autoload_register('PHPMailerAutoload');
-    }
-} else {
-    /**
-     * Fall back to traditional autoload for old PHP versions
-     * @param string $classname The name of the class to load
-     */
-    // Removed by Ivan Tcholakov, 09-JAN-2019.
-    //function __autoload($classname)
-    //{
-    //    PHPMailerAutoload($classname);
-    //}
-    //
-}
+spl_autoload_register('PHPMailerAutoload', true, true);

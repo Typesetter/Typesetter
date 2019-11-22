fc36eece2d137e165a230aa7977380bb3aa8ebfc
juek <jk@grafikrausz.at>
2019-11-22 22:54:20 +0100

    reformatting

--- include/tool.php
+++ include/tool.php
@@ -983,7 +983,7 @@ namespace gp{
 				'showsitemap'				=> true,
 				'showlogin'					=> true,
 				'auto_redir'				=> 90,			//2.5
-				'history_limit'				=> min(gp_backup_limit,30),
+				'history_limit'				=> min(gp_backup_limit, 30),
 				'resize_images'				=> true,		//3.5
 				'addons'					=> array(),
 				'themes'					=> array(),
@@ -1734,7 +1734,7 @@ namespace gp{
 		 * @param int $loops The number of times to loop the $arg through the algorithm
 		 *
 		 */
-		public static function hash($arg, $algo='sha512', $loops = 1000){
+		public static function hash( $arg, $algo='sha512', $loops = 1000){
 			$arg = trim($arg);
 
 			switch($algo){
@@ -1757,11 +1757,11 @@ namespace gp{
 
 			//sha512: looped with dynamic salt
 			for( $i=0; $i<$loops; $i++ ){
-				$ints			= preg_replace('#[a-f]#', '', $arg);
-				$salt_start		= (int)substr($ints, 0, 1);
-				$salt_len		= (int)substr($ints, 2, 1);
-				$salt			= substr($arg, $salt_start, $salt_len);
-				$arg			= hash($algo, $arg . $salt);
+				$ints			= preg_replace('#[a-f]#','',$arg);
+				$salt_start		= (int)substr($ints,0,1);
+				$salt_len		= (int)substr($ints,2,1);
+				$salt			= substr($arg,$salt_start,$salt_len);
+				$arg			= hash($algo,$arg.$salt);
 			}
 
 			return $arg;

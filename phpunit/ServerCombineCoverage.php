fcb1f9fc519b171f281091eb6f73b0fad72f8142
Josh Schmidt <oyejorge@gmail.com>
2019-12-11 18:59:46 -0700

    output debug info for coverage combine

--- phpunit/ServerCombineCoverage.php
+++ phpunit/ServerCombineCoverage.php
@@ -2,20 +2,31 @@
 
 require 'ServerPrepend.php';
 
-$cov_dir	= dirname(__DIR__).'/x_coverage';
-$files		= scandir($cov_dir);
+$cov_dir		= dirname(__DIR__).'/x_coverage';
+$files			= scandir($cov_dir);
+$file_count		= 0;
 
 foreach($files as $file){
-	
-	if( strpos($file,'request-') !== 0 ){
+
+	if( $file === '.' || $file === '..' ){
 		continue;
 	}
+
+	if( strpos($file,'request-') !== 0 ){
+		echo "\n - invalid coverage file: ".$file;
+		continue;
+	}
+
+	echo "\n - coverage file: ".$file;
 
 	$file	= $cov_dir.'/'.$file;
 	$data	= json_decode( file_get_contents($file),true );
 	$cov_obj->append($data, $file );
+	$file_count++;
 }
 
+echo "\n - ".$file_count.' coverage files combined in '.$cov_dir;
+echo "\n";
 
 $cov_file	= $cov_dir.'/requests.xml';
 $writer		= new \SebastianBergmann\CodeCoverage\Report\Clover;

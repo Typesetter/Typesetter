abc98452672b3b9883e482e567ccc08a0b1dfec4
Josh Schmidt <oyejorge@gmail.com>
2020-01-09 18:12:21 -0700

    Fix extra_edit.js

--- include/js/admin/extra_edit.js
+++ include/js/admin/extra_edit.js
@@ -1,23 +1,27 @@
 
 $(function(){
-	alert('here');
 
-	CKEDITOR.instances.gpcontent.on("change", function(){
-		if( CKEDITOR.instances.gpcontent.checkDirty() ){
-			$(".gp_publish_extra").hide();
-		}else{
-			$(".gp_publish_extra").show();
-		}
+	CKEDITOR.on( 'instanceCreated', function(evt){
+
+		evt.editor.on('change',function(){
+			if( evt.editor.checkDirty() ){
+				$('.gp_publish_extra').hide();
+			}else{
+				$('.gp_publish_extra').show();
+			}
+		});
+
+		$('.gp_save_extra').on('click', function(){
+			evt.editor.resetDirty();
+		});
+
+		$(window).on('beforeunload', function(){
+			if( evt.editor.checkDirty() ){
+				return 'Content was changed! Proceed anyway?';
+			}
+		});
+
 	});
 
-	$(".gp_save_extra").on("click", function(){
-		CKEDITOR.instances.gpcontent.resetDirty();
-	});
-
-	$(window).on("beforeunload", function(){
-		if( CKEDITOR.instances.gpcontent.checkDirty() ){
-			return "Content was changed! Proceed anyway?";
-		}
-	});
 
 });

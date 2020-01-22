f257974f3dae062cd44bb9064184a5c638acbf34
Josh Schmidt <oyejorge@gmail.com>
2020-01-22 14:27:30 -0700

    only get coverage if xdebug is avail

--- phpunit/ServerPrepend.php
+++ phpunit/ServerPrepend.php
@@ -10,18 +10,20 @@
 defined('gp_unit_testing') or define('gp_unit_testing',true);
 defined('gpdebug') or define('gpdebug',true); // sets error_reporting(ALL)
 
-xdebug_start_code_coverage();
+if( function_exists('xdebug_start_code_coverage') ){
+	xdebug_start_code_coverage();
 
-function SaveXdebugCoverage(){
-	$cov_dir	= dirname(__DIR__).'/x_coverage';
-	$name		= str_replace('/','-','request-'.$_SERVER['HTTP_X_REQ_ID'].'-'.$_SERVER['REQUEST_METHOD'].'-'.$_SERVER['REQUEST_URI']);
-	$cov_file	= $cov_dir.'/'.$name.'.json';
-	$data		= xdebug_get_code_coverage();
-	file_put_contents( $cov_file, json_encode($data,JSON_PRETTY_PRINT));
+	function SaveXdebugCoverage(){
+		$cov_dir	= dirname(__DIR__).'/x_coverage';
+		$name		= str_replace('/','-','request-'.$_SERVER['HTTP_X_REQ_ID'].'-'.$_SERVER['REQUEST_METHOD'].'-'.$_SERVER['REQUEST_URI']);
+		$cov_file	= $cov_dir.'/'.$name.'.json';
+		$data		= xdebug_get_code_coverage();
+		file_put_contents( $cov_file, json_encode($data,JSON_PRETTY_PRINT));
+	}
+
+	register_shutdown_function('SaveXdebugCoverage');
+	ob_start(function($buffer){
+		SaveXdebugCoverage();
+		return $buffer;
+	});
 }
-
-register_shutdown_function('SaveXdebugCoverage');
-ob_start(function($buffer){
-	SaveXdebugCoverage();
-	return $buffer;
-});

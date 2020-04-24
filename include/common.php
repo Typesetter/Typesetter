b0f945fc34c908c4174c418bcff831939d70d271
juek <jk@grafikrausz.at>
2020-04-24 18:33:18 +0200

    delay setting message style for resizing
    * give the browser a little more time for drawing the content

--- include/common.php
+++ include/common.php
@@ -441,11 +441,11 @@ function GetMessages( $wrap = true ){
 		$result .= '<ul>';
 		$result .= implode('',$wbMessageBuffer);
 		$result .= '</ul></div></div>';
-		$result .= '<script>(function(){';
+		$result .= '<script>(function(){setTimeout(function(){';
 		$result .= 'var elem = document.querySelectorAll(".messages > div")[0];';
 		$result .= 'elem.style.height = elem.offsetHeight + "px";';
 		$result .= 'elem.style.maxHeight = "calc(100vh - 40px)";';
-		$result .= '})();</script>';
+		$result .= '},150);})();</script>';
 	}
 
 	return $result .= \gp\tool::ErrorBuffer().$wrap_end;

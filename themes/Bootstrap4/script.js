c633bfe59cab5b3b5182fc7cb932814b674b4363
juek <jk@grafikrausz.at>
2020-08-29 13:17:36 +0200

    multi-level main nav
    * change main menu to 'All Links'
    * not using smartmenu but an own, more bootstrap-like implementation
    * submenu heading pages/entries work like 1st-level heading pages, which
    means that they only act as togglers and cannot be accessed
    
    My general recommendation: for heading pages use 'External Link' menu
    entries with a non-existing anchor id, like #heading-page
    Exception: with Multi-Language Manager, we need regular pages because
    MLM cannot translate external link titles

--- themes/Bootstrap4/script.js
+++ themes/Bootstrap4/script.js
@@ -10,6 +10,33 @@ $(function(){
 	$('[data-toggle="popover"]').popover();
 
 
+	// multi-level main nav
+	$('.dropdown-menu a.dropdown-toggle').on('click', function(e){
+		var $this = $(this);
+		var $parent_li = $this.parent('.dropdown');
+		var $next_ul = $this.next('.dropdown-menu');
+
+		var $toggle = !$this.hasClass('dropdown-active');
+
+		if( !$toggle ){
+			$parent_li.find('.dropdown-item').removeClass('dropdown-active');
+			$parent_li.find('.show').removeClass('show');
+		}
+
+		$this.toggleClass('dropdown-active', $toggle);
+		$parent_li.toggleClass('show', $toggle);
+		$next_ul.toggleClass('show', $toggle);
+
+		$this.parents('li.nav-item.dropdown.show')
+			.on('hidden.bs.dropdown', function(e){
+				$('.dropdown-menu .show').removeClass('show');
+				$this.removeClass('dropdown-active');
+			});
+
+		return false;
+	});
+
+
 	// make Simple Blog's Archive Gadget's years expandable and collapsed by default
 	if( $('.simple_blog_gadget_year').length ){
 		$('.simple_blog_gadget_year')

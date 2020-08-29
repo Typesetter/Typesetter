829ce597f205c71bb2e17e63468fd3c5301e3aa3
juek <jk@grafikrausz.at>
2020-08-29 10:52:15 +0200

    ident using tabs

--- themes/Bootstrap4/template.php
+++ themes/Bootstrap4/template.php
@@ -7,12 +7,19 @@
 
 
 /**
+ * Load SmartMenus
+ *
+ */
+// $page->head_js[] = dirname(rawurldecode($page->theme_path)) . '/_assets/js/jquery.smartmenus.bs4.min.js';
+
+
+/**
  * Load theme javascript, if it exists
  *
  */
 $theme_js = $page->theme_dir . '/script.js';
 if( file_exists($theme_js) ){
-  $page->head_js[] = dirname(rawurldecode($page->theme_path)) . '/script.js';
+	$page->head_js[] = dirname(rawurldecode($page->theme_path)) . '/script.js';
 }
 
 

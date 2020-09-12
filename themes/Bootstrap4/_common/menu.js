3a461bedb865b0dadfc98127386d21bcd643749d
juek <jk@grafikrausz.at>
2020-09-12 22:56:21 +0200

    fix open mobile menu window resize issues

--- themes/Bootstrap4/_common/menu.js
+++ themes/Bootstrap4/_common/menu.js
@@ -87,6 +87,10 @@ $(function(){
 				}
 				set_collapse_top();
 				$(window).on('scroll.themebs4.slideover', set_collapse_top);
+				$(document).one('navbar_expanded.themebs4.slideover', function(){
+					console.log('navbar_expanded - ' + Date.now());
+					$navbar_toggler.trigger('click');
+				});
 				$(this)
 					.addClass('main-nav-sliding-in')
 					.on('swipeRight.themebs4', function(evt, info){
@@ -105,10 +109,10 @@ $(function(){
 
 			$navbar_collapse.on('hidden.bs.collapse', function(evt){
 				$(this)
-					.removeClass('main-nav-sliding-out')
+					.removeClass('main-nav-sliding-in main-nav-sliding-out show')
 					.off('swipeRight.themebs4');
-
 				$(window).off('scroll.themebs4.slideover');
+				$(document).off('navbar_expanded.themebs4.slideover');
 			});
 			break;
 
@@ -119,13 +123,19 @@ $(function(){
 			$navbar_collapse.touch(); // init jquery-touch
 
 			$navbar_collapse.on('show.bs.collapse', function(evt){
-				var toggler_right = $(window).width() - $navbar_toggler.get(0).getBoundingClientRect().right;
+				var move_toggler = function(){
+					var toggler_right = $main_header.get(0).getBoundingClientRect().right -
+						$navbar_toggler.parent().get(0).getBoundingClientRect().right;
+					$navbar_toggler.css({
+						transform : 'translateX(' + toggler_right + 'px)'
+					});
+				}
+				move_toggler();
+				$(window).on('resize.themebs4', move_toggler);
+
 				var nav_width = $navbar_collapse.outerWidth() - 1; // console.log('w = ', $nav_width);
 				$('body').css({
 					left : '-' + nav_width + 'px'
-				});
-				$navbar_toggler.css({
-					transform : 'translateX(' + toggler_right + 'px)'
 				});
 				$(this)
 					.addClass('main-nav-canvas-on')
@@ -133,6 +143,10 @@ $(function(){
 						// console.log('jquery-touch info = ', info);
 						$navbar_toggler.trigger('click');
 					});
+				$(document).one('navbar_expanded.themebs4.offcanvas', function(){
+					console.log('navbar_expanded - ' + Date.now());
+					$navbar_toggler.trigger('click');
+				});
 			});
 
 			$navbar_collapse.on('shown.bs.collapse', function(evt){
@@ -141,7 +155,7 @@ $(function(){
 
 			$navbar_collapse.on('hide.bs.collapse', function(evt){
 				$('body').css({
-					'left' : '0'
+					left : 0
 				});
 				$navbar_toggler.css({
 					transform : 'translateX(0)'
@@ -151,8 +165,14 @@ $(function(){
 
 			$navbar_collapse.on('hidden.bs.collapse', function(evt){
 				$(this)
-					.removeClass('main-nav-canvas-off')
+					.removeClass('main-nav-canvas-on main-nav-canvas-off show')
 					.off('swipeRight.themebs4');
+				$navbar_toggler.removeAttr('style');
+				$('body').css({
+					left : 0
+				});
+				$(window).off('resize.themebs4');
+				$(document).off('navbar_expanded.themebs4.offcanvas');
 			});
 			break;
 
